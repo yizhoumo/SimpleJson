6454bd9ddde79b2cf88833773968920d722750d9
Rimo <yizhoumo@hotmail.com>
2020-02-14 00:11:43 +0800

    Fix: build error on MSVC

--- test/WriterTest.h
+++ test/WriterTest.h
@@ -1,8 +1,9 @@
 #ifndef SIMPLEJSON_WRITERTEST_H
 #define SIMPLEJSON_WRITERTEST_H
 
-#define EXPECT_ROUNDTRIP(expected)                   \
+#define EXPECT_ROUNDTRIP(doc)                        \
     do {                                             \
+        const auto expected = doc;                   \
         Value root;                                  \
         reader.parse(expected, root);                \
         ASSERT_EQ(ParseResult::Ok, reader.result()); \
